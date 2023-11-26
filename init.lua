2846df258f17c693231fe2604f7317a40de43a25
= <=>
2023-11-26 15:44:26 +0000

    Adds auto pairs plugin

--- init.lua
+++ init.lua
@@ -81,6 +81,8 @@ require('lazy').setup({
 
   'preservim/nerdtree',
 
+  'jiangmiao/auto-pairs',
+
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
